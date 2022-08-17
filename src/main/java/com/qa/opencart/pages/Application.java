42f2b27a70f3e3ccf40a98f030be8d4b63337496
Spoorthi Raj <spoorthiraj@Spoorthis-MacBook-Air.local>
2022-08-17 16:06:48 +0530

    Application class added

---
+++ src/main/java/com/qa/opencart/pages/Application.java
@@ -0,0 +1,9 @@
+package com.qa.opencart.pages;
+
+public class Application {
+    public static void main(String[] args) {
+
+        String app = "PlayStore";
+        System.out.println("most used app is,"+ app);
+    }
+}
