1edb570f04016fa384527d1fcd7b45511312cdac
Spoorthi Raj <spoorthiraj@Spoorthis-MacBook-Air.local>
2022-08-19 01:00:28 +0530

    fixed title login

--- src/main/java/com/qa/opencart/utills/Constants.java
+++ src/main/java/com/qa/opencart/utills/Constants.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Constants {
-    public static final String LOGIN_PAGE_TITLE ="Account Login1";
+    public static final String LOGIN_PAGE_TITLE ="Account Login";
     public static final String LOGIN_PAGE_URL_FRACTION = "route=account/login";
     public static final int DEFAULT_TIME_OUT = 5;
 
